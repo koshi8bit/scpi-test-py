e50b341a91ee079732120139e9909d70d8f58366
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2024-07-19 14:47:16 +0700

    cool power supply

--- src/main.py
+++ src/main.py
@@ -1,54 +1,33 @@
 from flask import Flask
 from waitress import serve
 import logging
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def root():
-    return """hello world <br><br>
-    <a href="/api/v1/channels">/api/v1/channels</a><br>
-    <a href="/api/v1/channels/elv">/api/v1/channels/elv</a><br>
-    <a href="/api/v1/channels/elv/dark">/api/v1/channels/elv/dark</a><br>
-    """
-
-
-@app.route("/api/v1/channels/")
-def api_v1_channels():
-    elv = {
-        "dark": 2.3,
-        "voltage": 980
-    }
-
-    result = {
-        "bergoz": 1.6,
-        "beam0": 1.3,
-        "elv": elv
-    }
-
-    return result
-
-
-@app.route("/api/v1/channels/elv")
-def api_v1_channels_elv():
-    elv = {
-        "dark": 2.3,
-        "voltage": 980
-    }
-
-    return elv
-
-
-@app.route("/api/v1/channels/elv/dark")
-def api_v1_channels_elv_dark():
-    return {"dark": 2.3}
-
+import easy_scpi as scpi
+import socket
+import time
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.WARNING,
                         format='%(asctime)s - %(levelname)s - %(message)s')
+
+    s = socket.socket()
+    s.connect(("10.3.20.12", 5025))
+    s.settimeout(3)
     logging.warning("init ok")
 
-    # serve(app, host='0.0.0.0', port=5053) USE IN PRODUCTION
-    app.run(host='0.0.0.0', port=5052)
+    s.send(str.encode("SYST:LOCK ON\n"))
+    s.send(str.encode("SOUR:CURR 1A\n"))
+    s.send(str.encode("SOUR:VOLT 1V\n"))
+    s.send(str.encode("OUTP ON\n"))
+    s.send(str.encode("*IDN?\n"))
+    print(s.recv(2048))
+    try:
+        while True:
+            s.send(str.encode("MEAS:CURR?\n"))
+            s.send(str.encode("MEAS:VOLT?\n"))
+            print(s.recv(2048))
+            time.sleep(1)
+    except KeyboardInterrupt:
+        s.send(str.encode("SOUR:CURR 0A\n"))
+        s.send(str.encode("SOUR:VOLT 0V\n"))
+        s.send(str.encode("OUTP OFF\n"))
+        print("PS stopped")
